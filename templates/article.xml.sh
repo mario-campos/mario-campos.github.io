b7acc4993e6f1dc1a00fbf886bb30d131afb5734
Mario Campos <mario.andres.campos@gmail.com>
2020-12-07 23:08:42 -0600

    correct templating for datetime
    sblg gets the date from the datetime attribute of the time tag

--- templates/article.xml.sh
+++ templates/article.xml.sh
@@ -1,12 +1,12 @@
 cat <<EOF
-<?xml version="1.0" encoding="UTF-8" ?>
+<?xml version="1.0" encoding="UTF-8"?>
 <article data-sblg-article="1">
   <header>
     <h1>$($LOWDOWN -X title "$1")</h1>
     <address>$($LOWDOWN -X author "$1")</address>
-    <time>$($LOWDOWN -X date "$1")</time>
-    <aside>$($LOWDOWN -X summary "$1")</aside>
+    <time datetime="$($LOWDOWN -X date "$1")">$($LOWDOWN -X date "$1")</time>
   </header>
+  <aside>$($LOWDOWN -X summary "$1")</aside>
   $($LOWDOWN "$1")
 </article>
 EOF
